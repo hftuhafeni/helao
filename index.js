513ce1552557863c9e262f8c658e0c99f45c33fe
vboxuser <vboxuser@Progrmmer.myguest.virtualbox.org>
2024-11-18 16:40:21 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const randomNumber =Math.floor(Math.random()*100);
+
+console.log(randomNumber);
+
+const readline =require('readline').createInterface({
+   input:process.stdin,
+  output:process.stdout
+
+});
+
+function guessNumber()
+{
+readline.question('Guess a number between 1-100:',(userGuess)=>{
+console.log(userGuess);
+
+const guess= parseInt(userGuess);
+
+ if(guess === randomNumber){
+ console.log("well done");
+ }
+ else{
+ console.log(" owapuka");
+ }
+})
+};
+
+guessNumber();
